15735462ef859780599f150a7c744d6e444285dd
Rustam Karimov <rustemk1981@gmail.com>
2019-09-14 20:44:37 +0200

    urls files added to each app

--- school_discipline/urls.py
+++ school_discipline/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('academic_year/', include(('academic_year.urls', 'academic_year'), namespace='years')),
+    path('discipline/', include(('discipline.urls', 'discipline'), namespace='discipline'))
+    path('grades/', include(('grades.urls', 'grades'), namespace='grades')),
+    path('settings/', include(('settings.urls', 'settings'), namespace='settings')),
+    path('teacher/', include(('users.urls.teacher_urls', 'users'), namespace='teacher')),
+    path('learner/', include(('users.urls.learner_urls', 'users'), namespace='learner')),
 ]
